23d7aec40399a55281825bb30949022a4195836f
Moha Hamra <mohah3000@gmail.com>
2025-10-01 09:57:23 +0200

    Añadidas funciones CRUD

--- Carreras.py
+++ Carreras.py
@@ -2,27 +2,50 @@ import utils
 
 class Carrera: 
     def  __init__(self, db, cursor):
-        #self.__db = db
-        #self.__cursor = cursor
-        ""
+        self.__db = db
+        self.__cursor = cursor
 
     def crear_carrera(self):
         nombre = input("Ingrese el nombre de la carrera: ")
-        sql = "insert into carrera (nombre) values (%s)"
-        """
-        self.__cursor.execute(sql, nombre)
+        sql = "INSERT INTO carrera (nombre) VALUES (%s)"
+        self.__cursor.execute(sql, (nombre,))
         self.__db.commit()
-        """
-        print(f"Creacion de carrera: {nombre}")
+        print(f"Carrera creada: {nombre}")
 
     def actualizar_carrera(self):
-        print("Logica de actualizar_carrera")
+        self.mostrar_carreras()
+        id_carrera = input("Ingrese el ID de la carrera a actualizar: ")
+        nuevo_nombre = input("Ingrese el nuevo nombre de la carrera: ")
+
+        sql = "UPDATE carrera SET nombre = %s WHERE id = %s"
+        self.__cursor.execute(sql, (nuevo_nombre, id_carrera))
+        self.__db.commit()
+        print(f"Carrera con ID {id_carrera} actualizada a: {nuevo_nombre}")
+
     def eliminar_carrera(self):
-        print("Logica de eliminar_carrera")
+        self.mostrar_carreras()
+        id_carrera = input("Ingrese el ID de la carrera a eliminar: ")
+
+        sql = "DELETE FROM carrera WHERE id = %s"
+        self.__cursor.execute(sql, (id_carrera,))
+        self.__db.commit()
+        print(f"Carrera con ID {id_carrera} eliminada.")
+
     def mostrar_carreras(self):
-        print("Logica de mostrar_carreras")
+        sql = "SELECT id, nombre FROM carrera"
+        self.__cursor.execute(sql)
+        carreras = self.__cursor.fetchall()
+
+        if carreras:
+            print("\nCarreras registradas:")
+            for id_carrera, nombre in carreras:
+                print(f"   {id_carrera} - {nombre}")
+        else:
+            print("No hay carreras registradas.")
+
 
     def menu(self):
+        from main import validar_numero
         opciones = {
             1: self.crear_carrera,
             2: self.actualizar_carrera,
