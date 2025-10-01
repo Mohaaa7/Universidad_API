295a078d9d77e7d7ab8c72815bfa8045e0b4de4e
Moha Hamra <mohah3000@gmail.com>
2025-10-01 12:37:31 +0200

    Creadas la clase Semestre con su constructor, getters, setters y método str

--- Semestres.py
+++ Semestres.py
@@ -1,80 +1,32 @@
-import utils
-
 class Semestre:
-    def  __init__(self, db, cursor):
-        self.__db = db
-        self.__cursor = cursor
 
-    def crear_semestre(self):
-        nombre = input("Ingrese el nombre del semestre: ")
-        id_carrera = input("Ingrese el id de la carrera: ")
-        sql = "INSERT INTO semestre (nombre, idSemestre) VALUES (%s, %s)"
-        self.__cursor.execute(sql, (nombre, id_carrera))
-        self.__db.commit()
-        print(f"Semestre creado: {nombre}")
+    def __init__(self, id_semestre, nombre, id_carrera):
+        self.__id_semestre = id_semestre
+        self.__nombre = nombre
+        self.__id_carrera = id_carrera
 
-    def actualizar_semestre(self):
-        self.mostrar_semestres()
-        id_semestre = utils.validar_numero("Ingrese el ID del semestre a actualizar: ")
-        nuevo_nombre = input("Ingrese el nuevo nombre del semestre: ")
-        nuevo_id_carrera = utils.validar_numero("Ingrese el nuevo id de la carrera: ")
+    # Getters
+    def get_id_semestre(self):
+        return self.__id_semestre
 
-        sql = "UPDATE semestre SET nombre = %s, idcarrera = %s WHERE idsemestre = %s"
-        self.__cursor.execute(sql, (nuevo_nombre, nuevo_id_carrera, id_semestre))
-        self.__db.commit()
-        if self.__cursor.rowcount == 0:
-            print("No se ha podido actualizar")
-        else:
-            print(f"Semestre con ID {id_semestre} actualizada")
+    def get_nombre(self):
+        return self.__nombre
 
-    def eliminar_semestre(self):
-        self.mostrar_semestres()
-        id_semestre = utils.validar_numero("Ingrese el ID del semestre a eliminar: ")
+    def get_id_carrera(self):
+        return self.__id_carrera
 
-        sql = "DELETE FROM semestre WHERE idsemestre = %s"
-        self.__cursor.execute(sql, (id_semestre,))
-        self.__db.commit()
-        if self.__cursor.rowcount == 0:
-            print("No se ha podido eliminar")
-        else:
-            print(f"Semestre con ID {id_semestre} eliminada.")
+    # Setters
+    def set_id_semestre(self, id_semestre):
+        self.__id_semestre = id_semestre
 
-    def mostrar_semestres(self):
-        sql = "SELECT * FROM semestre"
-        self.__cursor.execute(sql)
-        semestres = self.__cursor.fetchall()
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
 
-        if semestres:
-            print("\Semestres registrados:")
-            print("   ID - Nombre")
-            for idSemestre, nombre in semestres:
-                print(f"   {idSemestre} - {nombre}")
-        else:
-            print("No hay semestres registrados.")
-
-    def menu(self):
-        opciones = {
-            1: self.crear_semestre,
-            2: self.actualizar_semestre,
-            3: self.eliminar_semestre,
-            4: self.mostrar_semestres
-        }
-        while True:
-            opcion = utils.validar_numero(
-                "Seleccione una Opcion:\n"\
-                "   1.- Crear Semestre.\n"\
-                "   2.- Actualizar Semestre.\n"\
-                "   3.- Eliminar Semestre.\n"\
-                "   4.- Mostrar Semestres Disponibles.\n"\
-                "   0.- Volver.\n",
-                0,4)
-            if opcion == 0: return 
-            accion = opciones.get(opcion)
-            accion()
+    def set_id_carrera(self, id_carrera):
+        self.__id_carrera = id_carrera
 
 
+    def __str__(self):
+        return f"Semestre {self.__id_semestre}, {self.__nombre}, {self.__id_carrera}"
 
 
-
-
-            
