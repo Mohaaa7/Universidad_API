53e1d9e413904ff6bf2d06d56d56f2c29a2a6d68
Moha Hamra <mohah3000@gmail.com>
2025-10-03 10:00:48 +0200

    Añadido el script de la base de datos

---
+++ moha_haroon_db.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE  IF NOT EXISTS `moha_haroon` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `moha_haroon`;
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: moha_haroon
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table ` carrera`
+--
+
+DROP TABLE IF EXISTS ` carrera`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE ` carrera` (
+  `idcarrera` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idcarrera`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table ` carrera`
+--
+
+LOCK TABLES ` carrera` WRITE;
+/*!40000 ALTER TABLE ` carrera` DISABLE KEYS */;
+INSERT INTO ` carrera` VALUES (1,'informatica'),(2,'matematicas');
+/*!40000 ALTER TABLE ` carrera` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `semestre`
+--
+
+DROP TABLE IF EXISTS `semestre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `semestre` (
+  `idsemestre` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `idcarrera` int DEFAULT NULL,
+  PRIMARY KEY (`idsemestre`),
+  KEY `idcarrera_idx` (`idcarrera`),
+  CONSTRAINT `idcarrera` FOREIGN KEY (`idcarrera`) REFERENCES ` carrera` (`idcarrera`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `semestre`
+--
+
+LOCK TABLES `semestre` WRITE;
+/*!40000 ALTER TABLE `semestre` DISABLE KEYS */;
+INSERT INTO `semestre` VALUES (1,'Primer semestre',1),(2,'Segundo semestre',2);
+/*!40000 ALTER TABLE `semestre` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-03  9:58:43
